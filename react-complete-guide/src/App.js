6b87d9ff6f25e172359f8cf5112edbbd236e06fe
AVINASH WADHAWAN <86773629+avinashmint@users.noreply.github.com>
2022-01-23 00:03:01 +0530

    expense basic module completed by using components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,43 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expense from "./components/Expenses/Expense";
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      id: 1,
+      title: "Amul Butter 500gm",
+      date: new Date(2022, 1, 1),
+      amount: 235,
+    },
+    {
+      id: 2,
+      title: "Grofers Desi Ghee 1kg",
+      date: new Date(2022, 1, 11),
+      amount: 392,
+    },
+    {
+      id: 3,
+      title: "HMB Brown Bread",
+      date: new Date(2022, 1, 15),
+      amount: 40,
+    },
+    {
+      id: 4,
+      title: "Patanjali Doodh Biscuit",
+      date: new Date(2022, 1, 21),
+      amount: 10,
+    },
+    {
+      id: 5,
+      title: "Amul Butter 100gm",
+      date: new Date(2022, 1, 1),
+      amount: 49,
+    },
+  ];
+
   return (
     <div className="App">
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expense items={expenses} />
     </div>
   );
 }
